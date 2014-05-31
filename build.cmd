fae3634b2e91fbe8664fc6b758adad45aa4abe33
Rasmus Mikkelsen <r@smus.nu>
2014-05-31 19:05:33 +0200

    Fixed NuGet.exe download

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 rem @echo off
 
-powershell -Command "if ((Test-Path 'Source\Solutions\.nuget\NuGet.exe') -eq $false) {(New-Object System.Net.WebClient).DownloadFile('http://nuget.org/nuget.exe', 'Source\.nuget\NuGet.exe')}"
+powershell -Command "if ((Test-Path 'Source\Solutions\.nuget\NuGet.exe') -eq $false) {(New-Object System.Net.WebClient).DownloadFile('http://nuget.org/nuget.exe', 'Source\Solutions\.nuget\NuGet.exe')}"
 
 "Source\Solutions\.nuget\NuGet.exe" "install" "FAKE.Core" "-OutputDirectory" "Tools" "-ExcludeVersion" "-version" "2.13.1"
 "Source\Solutions\.nuget\NuGet.exe" "install" "NUnit.Runners" "-OutputDirectory" "Tools" "-ExcludeVersion" "-version" "2.6.3"
