9ab579725aa4219da5d1c34a772975cd286ff6a0
Rasmus Mikkelsen <r@smus.nu>
2014-06-05 22:53:25 +0200

    Remember to import PowerShell module

--- Source/Code/PowerUps.MicroServices.PushoverFacade/Deploy.ps1
+++ Source/Code/PowerUps.MicroServices.PushoverFacade/Deploy.ps1
@@ -3,5 +3,6 @@
 $MyDir = Split-Path $MyInvocation.MyCommand.Definition
 Import-Module $MyDir"\CiPsLib.Common.psm1" -Force
 Import-Module $MyDir"\CiPsLib.TopShelf.psm1" -Force
+Import-Module $MyDir"\PowerUps.MicroServices.Core.psm1" -Force
 
 Deploy-MicroServicesDefault
